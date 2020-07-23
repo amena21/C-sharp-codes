a39ec3e246a86e1d7a9697ca50cdb6b8cb0e852b
Rizoanun Nasa <rizoa58cv@gmail.com>
2020-07-23 15:40:29 +0600

    Find Square Root

---
+++ claculation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace claculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            double sqr = Math.Sqrt(num);
+            Console.WriteLine("The Square Root of" + " " + num + " is" + " " + sqr);
+            Console.ReadKey();
+        }
+    }
+}
+            
