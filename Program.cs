a77f125fca124b3de04d838dc83d92f0b30f1c80
amena21 <amena21@users.noreply.github.com>
2020-07-26 11:35:43 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the Hight of Triangle");
+            Double Hig = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Base of Triangle");
+            Double Bas = Convert.ToDouble(Console.ReadLine());
+            Double Area = Convert.ToDouble(0.5 * (Hig * Bas));
+
+            Console.WriteLine("The Are of Triangle is " + Area);
+            Console.ReadKey();
+        }
+    }
+}
